86f6f03dc98ee3f0fb4efc2581c60ac91d6f1877
JaeEon <jae1321@naver.com>
2025-08-12 21:45:03 +0900

    글목록 조회기능 만들기(DB 출력)

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ export default async function Home() {
 
   let db = (await connectDB).db('forum');
   let result = await db.collection('post').find().toArray();
-  console.log(result);
 
   return (
     <div>안녕</div>
