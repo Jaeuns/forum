41d5e294a0ac01199728951c785c6e23e7f6aa78
JaeEon <jae1321@naver.com>
2025-08-12 20:08:13 +0900

    몽고DB연결

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
-export default function Home() {
+import { connectDB } from '@/util/database.js';
+
+export default async function Home() {
+
+  let db = (await connectDB).db('forum');
+  let result = await db.collection('post').find().toArray();
+  console.log(result);
+
   return (
-    <div>안녕!</div>
+    <div>안녕</div>
   )
 }
