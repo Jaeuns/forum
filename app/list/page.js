35087403d31a449e7b7b0abf3b1640cca2c630fd
JaeEon <jae1321@naver.com>
2025-08-13 12:10:48 +0900

    수정기능 만들기 1

--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,5 @@
 import { connectDB } from '@/util/database';
 import Link from 'next/link';
-import DetailLink from './DetailLink';
 
 export default async function List() {
   const db = (await connectDB).db('forum');
@@ -10,10 +9,10 @@ export default async function List() {
       {
         result.map((a, i) => 
           <div className="list-item" key={i}>
-            <Link prefetch={false} href={'/detail/' + result[i]._id}>
+            <Link href={'/detail/' + result[i]._id}>
               <h4>{result[i].title}</h4>
             </Link>
-            <DetailLink />
+            <Link href={'/edit/' + result[i]._id}>✏️</Link>
             <p>1월 1일</p>
           </div>
         )
